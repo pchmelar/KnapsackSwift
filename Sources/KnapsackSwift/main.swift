19ace6084c4b9d3240a02a5c679625e319b84307
Petr Chmelar <info@pchmelar.cz>
2017-10-25 22:25:27 +0200

    main.swift improved

--- Sources/KnapsackSwift/main.swift
+++ Sources/KnapsackSwift/main.swift
@@ -9,9 +9,7 @@
 import Foundation
 
 // parse arguments
-if CommandLine.arguments.count != 2 {
-    print("Usage: KnapsackSwift inputFile")
-} else {
+if CommandLine.arguments.count == 3, let met = Int(CommandLine.arguments[2]), met > 0, met < 3 {
 
 		// determine full path of inputFile
 		var fullPath = ""
@@ -31,30 +29,30 @@ if CommandLine.arguments.count != 2 {
         let inputFile = try String(contentsOf: URL(fileURLWithPath: fullPath), encoding: .utf8)
         let lines = inputFile.components(separatedBy: .newlines)
 
-        // solve with brute force
-        var start = NSDate()
+        let start = NSDate()
         for line in lines {
         		if line != "" {
         				let problem = ProblemInstance(input: line)
-        				let bestValue = problem.solveBruteForce(n: problem.size-1, currentWeight: 0, currentValue: 0)
-        				//print("Best value of problem instance #\(problem.id) is \(bestValue)")
-        		}
-        }
-        print("Brute force method evaluation time: \(abs(start.timeIntervalSinceNow))")
 
-        // solve with heuristic
-        start = NSDate()
-        for line in lines {
-        		if line != "" {
-        				let problem = ProblemInstance(input: line)
-        				let bestValue = problem.solveHeuristic()
+        				// solve with specified method
+        				var bestValue: Int = 0
+        				switch met {
+        				case 1:
+        						bestValue = problem.solveBruteForce(n: problem.size-1, currentWeight: 0, currentValue: 0)
+        				case 2:
+        						bestValue = problem.solveHeuristic()
+        				default:()
+        				}
         				//print("Best value of problem instance #\(problem.id) is \(bestValue)")
         		}
         }
-        print("Heuristic method evaluation time: \(abs(start.timeIntervalSinceNow))")
+        print("Evaluation time: \(abs(start.timeIntervalSinceNow))")
     }
     catch {
     		print("Input file corrupted")
     }
 
+} else {
+	  print("Usage: KnapsackSwift inputFile method")
+    print("Methods: 1 for brute force, 2 for heuristic")
 }
